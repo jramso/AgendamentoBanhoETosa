8b4b5f6019f565df7228f6df675f37a9c44c714d
Daniel Pinheiro <101371419+danielpinheiro2@users.noreply.github.com>
2024-11-11 15:13:18 -0300

    Atualização
    att Agendamento e Servico

--- AgendamentoBanhoETosa/Model/Agendamento.cs
+++ AgendamentoBanhoETosa/Model/Agendamento.cs
@@ -2,5 +2,15 @@
 {
     public class Agendamento
     {
+        public int Id { get; set; }
+        public DateTime DataHora { get; set; }
+        public int ClienteId { get; set; }
+        public Cliente Cliente { get; set; }
+        public int PetId { get; set; }
+        public Pet Pet { get; set; }
+        public int ServicoId { get; set; }
+        public Servico Servico { get; set; }
+
     }
+
 }
--- AgendamentoBanhoETosa/Model/Servico.cs
+++ AgendamentoBanhoETosa/Model/Servico.cs
@@ -2,5 +2,9 @@
 {
     public class Servico
     {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public float Preco { get; set; }
+
     }
 }
