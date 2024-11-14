c2c29b79fe81089a19f36f2b7851d0b3be8838a3
jramso <josue.rsou2@gmail.com>
2024-11-14 00:25:37 -0300

    Atualização montando o serviço de agendamento

---
+++ AgendamentoBanhoETosa/Services/AgendamentoServ.cs
@@ -0,0 +1,19 @@
+﻿using AgendamentoBanhoETosa.IRepositories;
+using AgendamentoBanhoETosa.Model;
+
+namespace AgendamentoBanhoETosa.Services
+{
+    public class AgendamentoServ
+    {
+        private readonly IAgendamento _agendamentoServ;
+        public AgendamentoServ(IAgendamento agendamentoServ )
+        {
+            _agendamentoServ = agendamentoServ;
+        }
+
+        public async Task<IEnumerable<Agendamento>> GetAgendamentos()
+        {
+            return await _agendamentoServ.GetAllAsync();
+        }
+    }
+}
