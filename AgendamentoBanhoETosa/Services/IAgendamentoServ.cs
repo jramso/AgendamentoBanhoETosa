d98ce8618b12ea95970b3eeea4b942c6212d2e9e
Daniel Pinheiro <101371419+danielpinheiro2@users.noreply.github.com>
2024-11-14 00:34:24 -0300

    Atualização: adicionando interface do serviço de agendamento

---
+++ AgendamentoBanhoETosa/Services/IAgendamentoServ.cs
@@ -0,0 +1,15 @@
+using AgendamentoBanhoETosa.Model;
+
+namespace AgendamentoBanhoETosa.Services
+{
+  public interface IAgendamentoServ
+  {
+      Task<IEnumerable<Agendamento>> GetAllAgendamentosAsync();        
+      Task<Agendamento?> GetAgendamentoByIdAsync(int id);              
+      Task CreateAgendamentoAsync(Agendamento agendamento);            
+      Task UpdateAgendamentoAsync(Agendamento agendamento);            
+      Task DeleteAgendamentoAsync(int id);                             
+      Task<IEnumerable<Agendamento>> GetAgendamentosByClienteAsync(int clienteId);  
+      Task<IEnumerable<Agendamento>> GetAgendamentosByDateAsync(DateTime date);     
+  }
+}
