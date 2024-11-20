27b963a5e7f3c852fa00919bc9b3c3ca5f0eada3
Daniel Pinheiro <101371419+danielpinheiro2@users.noreply.github.com>
2024-11-20 14:21:53 -0300

    Criação do Pet Service e Interface PetService

---
+++ AgendamentoBanhoETosa/Services/IPetServ.cs
@@ -0,0 +1,26 @@
+﻿using AgendamentoBanhoETosa.Model;
+
+
+namespace AgendamentoBanhoETosa.Services
+{
+    public interface IPetServ
+    {
+
+        // Método para obter todos os pets
+        Task<IEnumerable<Pet>> GetAllPetsAsync();
+
+        // Método para obter um pet pelo ID
+        Task<Pet?> GetPetByIdAsync(int id);
+
+        // Método para adicionar um novo cliente
+        Task<Pet> AddPetAsync(Pet pet);
+
+        // Método para atualizar um pet existente
+        Task<bool> UpdatePetAsync(int id, Pet petAtualizado);
+
+        // Método para excluir um pet pelo ID
+        Task<bool> DeletePetAsync(int id);
+
+
+    }
+}
---
+++ AgendamentoBanhoETosa/Services/PetServ.cs
@@ -0,0 +1,68 @@
+﻿using AgendamentoBanhoETosa.Data;
+using AgendamentoBanhoETosa.Model;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace AgendamentoBanhoETosa.Services
+{
+    public class PetServ : IPetServ
+    {
+        private readonly AppDbContext _dbContext;
+        public PetServ(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        //obter todos os pets
+        public async Task<IEnumerable<Pet>> GetAllPetsAsync()
+        {
+            return await _dbContext.Pets.ToListAsync();
+        }
+
+        // Obtém um pet pelo ID
+        public async Task<Pet?> GetPetByIdAsync(int id)
+        {
+            return await _dbContext.Pets.FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        // Adiciona um novo pet
+        public async Task<Pet> AddPetAsync(Pet pet)
+        {
+            _dbContext.Pets.Add(pet);
+            await _dbContext.SaveChangesAsync();
+            return pet;
+        }
+
+        // Atualiza um pet existente
+        public async Task<bool> UpdatePetAsync(int id, Pet petAtualizado)
+        {
+            var pet = await _dbContext.Pets.FindAsync(id);
+            if (pet == null)
+            {
+                return false; // Pet não encontrado
+            }
+
+            // Atualiza os campos necessários
+            pet.Nome = petAtualizado.Nome;
+            pet.Tipo = petAtualizado.Tipo;
+
+            await _dbContext.SaveChangesAsync();
+            return true;
+        }
+
+        // Exclui um pet pelo ID
+        public async Task<bool> DeletePetAsync(int id)
+        {
+            var pet = await _dbContext.Pets.FindAsync(id);
+            if (pet == null)
+            {
+                return false; // pet não encontrado
+            }
+
+            _dbContext.Pets.Remove(pet);
+            await _dbContext.SaveChangesAsync();
+            return true;
+        }
+
+    }
+}
