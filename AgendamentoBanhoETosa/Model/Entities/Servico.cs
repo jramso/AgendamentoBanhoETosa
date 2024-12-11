a970cb102e193d9e598776d79e7b3d5aed4926b6
Josué Ramos Souza <80490332+jramso@users.noreply.github.com>
2024-12-11 09:09:46 -0300

    feat: adicionando configuração para uso de DTO

---
+++ AgendamentoBanhoETosa/Model/Entities/Servico.cs
@@ -0,0 +1,10 @@
+namespace AgendamentoBanhoETosa.Model
+{
+    public class Servico
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public float Preco { get; set; }
+
+    }
+}
