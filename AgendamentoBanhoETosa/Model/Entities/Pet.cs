73c7f5eecd54e5642f55691fe09760cc41586205
Daniel Pinheiro <101371419+danielpinheiro2@users.noreply.github.com>
2024-12-15 12:01:40 -0300

    CreateEnum

--- AgendamentoBanhoETosa/Model/Entities/Pet.cs
+++ AgendamentoBanhoETosa/Model/Entities/Pet.cs
@@ -1,12 +1,15 @@
+using AgendamentoBanhoETosa.Model.Enums;
+
 namespace AgendamentoBanhoETosa.Model
 {
     public class Pet
     {
         public int Id { get; set; }
         public string Nome { get; set; }
-        public string Tipo { get; set; }
+        public TipoPet Tipo { get; set; }
+        public RacaCachorro? RacaCachorro { get; set; }
+        public RacaGato? RacaGato { get; set; }
         public int ClienteId { get; set; }
-
-        //public Cliente Cliente { get; set; }   
     }
 }
+
---
+++ AgendamentoBanhoETosa/Model/Enums/ETipoPet.cs
@@ -0,0 +1,8 @@
+namespace AgendamentoBanhoETosa.Model.Enums
+{
+    public enum TipoPet
+    {
+        Gato,
+        Cachorro
+    }
+}
